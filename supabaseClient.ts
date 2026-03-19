c4c3183d5088f71c3d52a6d39ff5dda7f9dbb545
Anhelina <anhelina0617@gmail.com>
2026-03-19 16:53:32 +0100

    fix: moved supabase key

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = "https://bjtjncvcncjrzghtyiok.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqdGpuY3ZjbmNqcnpnaHR5aW9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNTk0NDcsImV4cCI6MjA4ODczNTQ0N30.cwoJ1UBxmF3N210dXtlAlXOEw6v_naLW27vJubUEVZw";
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_KEY!;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
